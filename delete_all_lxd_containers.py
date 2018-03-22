913c52affeb9eb84b76db646a3790a3a760f4d6c
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2018-03-22 11:23:47 -0400

    Add image delete option and use arguments to determine image or container

--- delete_all_lxd_containers.py
+++ delete_all_lxd_containers.py
@@ -1,34 +1,89 @@
 #!/usr/bin/env python3
-""" This script deletes all LXC containers running on a machine
+""" This script deletes all LXC containers and/or images.
 
-All containers are deleted, including running containers.
+All containers and images are deleted. This incldes running containers
+and OS images.
 
-The script assumes that the user running the script has non-sudo access to lxc
+The script assumes that the user running the script has non-sudo access
+to lxc
 
 No dependencies.
 
-The command that is run: lxc delete --force <container_name>.
-
 Author: Ilhaan Rasheed
-Date: March 6th, 2018
 """
 
 import json
 from pprint import pprint
 import subprocess
+import argparse
+import sys
 
-# Run lxc list command to get JSON output as a string
-lxc_output = subprocess.run(["lxc", "list", "-c", "n", "--format=json"],
-                            stdout=subprocess.PIPE).stdout.decode('utf-8')
 
-# Load json string output
-data = json.loads(lxc_output)
+def get_args():
+    """Get args from command line"""
 
-# Create empty list to store LXC Container names
-container_names = []
-for x in range(len(data)):
-    container_names.append(data[x]["name"])
+    parser = argparse.ArgumentParser(description='Delete LXC Containers and \
+                                                  Images')
+    parser.add_argument('-c', '--containers', dest='container_delete',
+                        default=False, action='store_true',
+                        help='Enable this to delete all LXC containers')
+    parser.add_argument('-i', '--images', dest='image_delete', default=False,
+                        action='store_true',
+                        help='Enable this to delete all LXC images')
+    args = parser.parse_args()
 
-# Run LXD Delete command for each container
-for container in container_names:
-    subprocess.run(["lxc", "delete", "--force", container])
+    if args.container_delete is False and args.image_delete is False:
+        print("You need to enable either container or images or both. \
+               Use --help for more info.")
+        sys.exit()
+    else:
+        return (args.container_delete, args.image_delete)
+
+
+def container_delete():
+    """Function that deletes all containers"""
+
+    # Run lxc list command to get JSON output as a string
+    lxc_output = subprocess.run(["lxc", "list", "-c", "n", "--format=json"],
+                                stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    # Load json string output
+    data = json.loads(lxc_output)
+
+    # Create empty list to store LXC Container names
+    container_names = []
+    for x in range(len(data)):
+        container_names.append(data[x]["name"])
+
+    # Run LXD Delete command for each container
+    for container in container_names:
+        subprocess.run(["lxc", "delete", "--force", container])
+
+
+def image_delete():
+    """Function that deletes all images"""
+
+    # Run lxc image list command to get JSON output as a string
+    lxc_output = subprocess.run(["lxc", "image", "list", "--format=json"],
+                                stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    # Load json string output
+    data = json.loads(lxc_output)
+
+    # Create empty list to store LXC Image fingerprints
+    image_fingerprints = []
+    for x in range(len(data)):
+        image_fingerprints.append(data[x]["fingerprint"])
+
+    # Run LXD Image Delete command for each container
+    for image in image_fingerprints:
+        subprocess.run(["lxc", "image", "delete", image])
+
+
+if __name__ == '__main__':
+    (container, image) = get_args()
+
+    if container:
+        container_delete()
+    if image:
+        image_delete()
